e737f9b6bef2193ff3ffe5c81a5caa35bf214658
ZeinHome <zein3288@gmail.com>
2022-07-13 20:45:47 +0300

    хочу плакать

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,64 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Box } from './App.styled';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
+import fetchImages from './Services/Services';
 
 export function App() {
   const [imageName, setImageName] = useState('');
+  const [imagesArray, setImagesArray] = useState([]);
+  const [pages, setPages] = useState(1);
+  const [isloading, setIsloading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState(null);
+  const [tags, setTags] = useState(null);
+
+  useEffect(() => {
+    const ImagesDate = fetchImages(imageName, pages);
+    if (imageName === '') {
+      return;
+    }
+    setIsloading(true);
+
+    ImagesDate.then(res => {
+      return setImagesArray(state => [...state, ...res.hits]);
+    }).finally(setIsloading(false));
+  }, [imageName, pages]);
+
+  const loadMore = () => {
+    setPages(prevState => prevState + 1);
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const handleImageClick = ({ largeImageURL, tags }) => {
+    toggleModal();
+
+    return setLargeImageURL(largeImageURL, setTags(tags));
+  };
 
   const handelSubmitForm = image => {
+    setImagesArray([]);
     setImageName(image);
+    setPages(1);
   };
   return (
     <Box>
       <Searchbar onSubmit={handelSubmitForm} />
-      <ImageGallery imageName={imageName} />
+      <ImageGallery
+        imagesArray={imagesArray}
+        isloading={isloading}
+        showModal={showModal}
+        largeImageURL={largeImageURL}
+        tags={tags}
+        handleImageClick={handleImageClick}
+        toggleModal={toggleModal}
+        loadMore={loadMore}
+      />
       <ToastContainer />
     </Box>
   );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,46 +1,19 @@
-import { useEffect, useState } from 'react';
 import { ImageGalleryContainer, Container } from './ImageGallery.styled';
 import ImageGalleryItems from '../ImageGalleryItems/ImageGalleryItems ';
 import Button from '../Button/Button';
 import Loader from '../Loader/Loader';
 import Modal from '../Modal/Modal';
-import fetchImages from '../Services/Services';
 
-export default function ImageGallery({ imageName }) {
-  const [imagesArray, setImagesArray] = useState([]);
-  const [pages, setPages] = useState(1);
-  const [isloading, setIsloading] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [largeImageURL, setLargeImageURL] = useState(null);
-  const [tags, setTags] = useState(null);
-
-  useEffect(() => {
-    const ImagesDate = fetchImages(imageName, pages);
-    if (imageName === '') {
-      return;
-    }
-
-    setIsloading(true);
-
-    ImagesDate.then(res => {
-      return setImagesArray(state => [...state, ...res.hits]);
-    }).finally(setIsloading(false));
-  }, [imageName, pages]);
-
-  const loadMore = () => {
-    setPages(prevState => prevState + 1);
-  };
-
-  const toggleModal = () => {
-    setShowModal(!showModal);
-  };
-
-  const handleImageClick = ({ largeImageURL, tags }) => {
-    toggleModal();
-
-    return setLargeImageURL(largeImageURL, setTags(tags));
-  };
-
+export default function ImageGallery({
+  imagesArray,
+  isloading,
+  showModal,
+  largeImageURL,
+  tags,
+  toggleModal,
+  handleImageClick,
+  loadMore,
+}) {
   return (
     <Container>
       {isloading && <Loader />}
